2332269ff95571668a94add4d8dc277aee4bbbd3
EvangelineNatashia <evangelinenatashia26@gmail.com>
2023-12-04 22:47:06 +0700

    Update Test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,10 +8,14 @@
 </head>
 <body>
     <h1>test 12345123456789</h1>
+
+    <h2>maca cii maca cii hai</h2>
+    <a href="http://"></a>
+    <h3>hohohhohohohohohohohohohoho</h3></body>
     <p>
         pada suatu hari ada seorang anak bernama Aryo bBintang Kecil. Dia sangat happy karena bisa beli telur gulung.
     </p>
     <h1>Aryo ganteng sekali</h1>
     <h2>Aryo ganteng banget</h2>
-</body>
+
 </html>
